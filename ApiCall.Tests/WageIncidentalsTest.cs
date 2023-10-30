af6c1032516bf602dae394a515be29ed3c0d8729
Nicolas Scheidt <nicolas.scheidt@voliro.com>
2023-10-30 09:49:59 +0100

    Cleanup navbar, rename tests

--- ApiCall.Tests/WageIncidentalsTest.cs
+++ ApiCall.Tests/WageIncidentalsTest.cs
@@ -4,14 +4,13 @@ using System.Diagnostics;
 using BlazorApp.Shared;
 
 
-public class UnitTest1
+public class WageIncidentalsTest
 {
     [Fact]
-    public async void Test1()
+    public async void TestSalaryUpdate()
     {
         WageIncidentals wageIncidentals = new WageIncidentals(31, 5, 100);
         bool result = await wageIncidentals.Contractor.UpdateSalaryBase(100000);
-        Debug.WriteLine(wageIncidentals.Contractor.SalaryTaxable);
         Assert.True(result);
         Assert.True(Math.Abs(59184.0 - wageIncidentals.Contractor.SalaryNet) < 1);
     }
